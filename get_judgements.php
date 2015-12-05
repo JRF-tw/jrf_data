97c5907100eac298526646df1c756ae7b811f162
billy3321 <billy3321@gmail.com>
2015-12-06 00:58:07 +0800

    fix some params

--- get_judgements.php
+++ get_judgements.php
@@ -7,7 +7,6 @@ $keyword = urlencode('號');
 $button = urlencode('查詢');
 $sel_judword = urlencode('常用字別');
 
-
 $court_json_content = file_get_contents("./courts.json");
 $courts_array = json_decode($court_json_content, true);
 
@@ -19,25 +18,19 @@ $edate -> modify('-1911 years');
 
 
 $sdate_string = ltrim($sdate -> format('Ymd'), '0');
-$sdate_year = ltrim($sdate -> format('Y'), '0');
-$sdate_month = ltrim($sdate -> format('m'), '0');
-$sdate_day = ltrim($sdate -> format('d'), '0');
 $edate_string = ltrim($edate -> format('Ymd'), '0');
-$edate_year = ltrim($edate -> format('Y'), '0');
-$edate_month = ltrim($edate -> format('m'), '0');
-$edate_day = ltrim($edate -> format('d'), '0');
 
 foreach ($courts_array as $court) {
-    $court_string = urlencode($court['name']);
+    $court_string = strtolower(urlencode($court['name']));
     foreach ($court['departments'] as $department) {
-        $url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx?&v_court={$court['code']}+{$court_string}&v_sys={$department['code']}&jud_year=&jud_case=&jud_no=&jud_title={$keyword}&keyword=&sdate={$sdate_string}&edate={$edate_string}&page=1&searchkw=";
+        $url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx?&v_court={$court['code']}+{$court_string}&v_sys={$department['code']}&jud_year=&jud_case=&jud_no=&jud_no_end=&jud_title=&keyword=&sdate={$sdate_string}&edate={$edate_string}&page=1&searchkw=&jmain=&cw=0";
         error_log($url);
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_REFERER, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $content = curl_exec($curl);
         curl_close($curl);
-        if (!preg_match('#本次查詢結果共([0-9]*)筆#', $content, $matches)) {
+        if (!preg_match('#共([0-9]*)筆#', $content, $matches)) {
             var_dump($content);
             error_log("{$court['name']} {$department['name']} has no record");
             continue;
