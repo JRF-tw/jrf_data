4a8dc80f613b0d949b291e93a8d4fbd544e909bf
billy3321 <billy3321@gmail.com>
2015-12-06 20:26:05 +0800

    now the php will write to database

--- get_judgements.php
+++ get_judgements.php
@@ -3,16 +3,23 @@
   參數為欲抓取的日期，如 2015-12-05。
 */
 date_default_timezone_set('Asia/Taipei');
+libxml_use_internal_errors(true);
 $keyword = urlencode('年');
 $button = urlencode('查詢');
 $sel_judword = urlencode('常用字別');
+$dbname = 'judgements-development';
+$dbuser = 'root';
+$password = 'P@ssw0rd';
 
 $court_json_content = file_get_contents("./courts.json");
 $courts_array = json_decode($court_json_content, true);
 
+$dsn = "mysql:host=127.0.0.1;dbname={$dbname}";
+$dbh = new PDO($dsn, $dbuser, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8;"));
+
 $sdate = new DateTime($argv[1]);
 $edate = new DateTime($argv[1]);
-$edate -> modify('+1 day');
+$sdate -> modify('-1 day');
 
 $sdate_string = ltrim($sdate -> format('Ymd'), '0');
 $edate_string = ltrim($edate -> format('Ymd'), '0');
@@ -29,14 +36,13 @@ foreach ($courts_array as $court) {
         $content = curl_exec($curl);
         curl_close($curl);
         if (!preg_match('#本次查詢結果共([0-9]*)筆#', $content, $matches)) {
-            var_dump($content);
             error_log("{$court['name']} {$department['name']} has no record");
             continue;
         }
         $count = $matches[1];
         if (!preg_match('#FJUDQRY03_1\.aspx\?id=[0-9]*&([^"]*)#', $content, $matches)){
-            var_dump($content);
-            throw new Exception('test2');
+            error_log('page seems errror.');
+            continue;
         }
         $param = $matches[1];
         for ($j = 1; $j <= $count; $j ++) {
@@ -50,7 +56,8 @@ foreach ($courts_array as $court) {
 
             if (!preg_match('#href="([^"]*)">友善列印#', $content, $matches)) {
                 var_dump($content);
-                throw new Exception('test3');
+                error_log('cannot find link');
+                continue;
             }
             $print_url = $matches[1];
             $query = parse_url($print_url, PHP_URL_QUERY);
@@ -73,9 +80,102 @@ foreach ($courts_array as $court) {
             ["jcheck"]=>
             string(1) "1"
              */
-            $court = explode(' ', $ret['v_court'])[0];
-            $file_name = "output/{$court}-{$ret['v_sys']}-{$ret['jdate']}-{$ret['jyear']}-{$ret['jcase']}-{$ret['jno']}-{$ret['jcheck']}";
-            file_put_contents($file_name, $content);
+            $court_code = explode(' ', $ret['v_court'])[0];
+            $court_name = explode(' ', $ret['v_court'])[1];
+            $date_string = explode(',', $ret['jrecno'])[3];
+            $datetime = new DateTime($date_string);
+            $date_string = $datetime->format("Y-m-d");
+            $content = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $content);
+            $doc = new DOMDocument;
+            $doc->loadHTML($content);
+            $pre_dom = $doc->getElementsByTagNAme('pre');
+            $judgement_content = $html = $pre_dom->item(0)->nodeValue;
+            $td_dom = $doc->getElementsByTagNAme('td');
+            $reason = $td_dom->item(10)->nodeValue;
+            $identify = $court_code . '-' . str_replace(",","-",$ret['jrecno']);
+            $now = new DateTime();
+            $now = $now->format("Y-m-d H:i:s");
+            switch ($ret['v_sys']) {
+                case 'V':
+                    $department = '民事';
+                    break;
+                case 'C':
+                    $department = '刑事';
+                    break;
+                case 'A':
+                    $department = '行政';
+                    break;
+                case 'P':
+                    $department = '公懲';
+                    break;
+                default:
+                    $department = null;
+                    break;
+            }
+            // $record = array(
+            //     $identify,
+            //     $court_code,
+            //     $court_name,
+            //     $ret['jyear'],
+            //     $ret['jcase'],
+            //     $ret['jno'],
+            //     $department,
+            //     $ret['jcheck'],
+            //     $reason,
+            //     $judgement_content,
+            //     $date_string,
+            //     $now,
+            //     $now
+            // );
+            $sql = "
+               INSERT INTO
+                  `judgements`
+               SET
+                  identify = :identify,
+                  court_code = :court_code,
+                  court_name = :court_name,
+                  year = :year,
+                  jcase = :jcase,
+                  jno = :jno,
+                  department = :department,
+                  jcheck = :jcheck,
+                  reason = :reason,
+                  content = :content,
+                  published_at = :published_at,
+                  created_at = :created_at,
+                  updated_at = :updated_at
+               ON DUPLICATE KEY UPDATE
+                  identify = :identify,
+                  court_code = :court_code,
+                  court_name = :court_name,
+                  year = :year,
+                  jcase = :jcase,
+                  jno = :jno,
+                  department = :department,
+                  jcheck = :jcheck,
+                  reason = :reason,
+                  content = :content,
+                  published_at = :published_at,
+                  updated_at = :updated_at
+            ";
+
+            $sth = $dbh->prepare($sql);
+            $sth->bindValue('identify', $identify , PDO::PARAM_STR);
+            $sth->bindValue('court_code', $court_code , PDO::PARAM_STR);
+            $sth->bindValue('court_name', $court_name , PDO::PARAM_STR);
+            $sth->bindValue('year', $year , PDO::PARAM_INT);
+            $sth->bindValue('jcase', $ret['jcase'] , PDO::PARAM_STR);
+            $sth->bindValue('jno', $ret['jno'] , PDO::PARAM_INT);
+            $sth->bindValue('department', $department , PDO::PARAM_STR);
+            $sth->bindValue('jcheck', $ret['jcheck'] , PDO::PARAM_STR);
+            $sth->bindValue('reason', $reason , PDO::PARAM_STR);
+            $sth->bindValue('content', $judgement_content , PDO::PARAM_STR);
+            $sth->bindValue('published_at', $date_string , PDO::PARAM_STR);
+            $sth->bindValue('updated_at', $now , PDO::PARAM_STR);
+            $sth->bindValue('created_at', $now , PDO::PARAM_STR);
+            $sth->execute();
+            // $file_name = "output/{$court_code}-{$ret['v_sys']}-{$ret['jdate']}-{$ret['jyear']}-{$ret['jcase']}-{$ret['jno']}-{$ret['jcheck']}";
+            // file_put_contents($file_name, $content);
         }
     }
 }
