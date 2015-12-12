7efdb205057dbf5c57e26a7c3940c5d1078af868
billy3321 <billy3321@gmail.com>
2015-12-13 01:56:00 +0800

    fix script error

--- get_year_judgements.sh
+++ get_year_judgements.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-DATE=gdate
+DATE=date
 FORMAT="%Y-%m-%d"
 start=`$DATE +$FORMAT -d "${1}-01-01"`
 end=`$DATE +$FORMAT -d "${1}-12-31"`
@@ -10,5 +10,5 @@ now=$start
 while [[ "$now" < "$end" ]] ; do
   now=`$DATE +$FORMAT -d "$now + 1 day"`
   echo "$now"
-  ./get_judgements "$now"
+  ./get_judgements.rb "$now"
 done
