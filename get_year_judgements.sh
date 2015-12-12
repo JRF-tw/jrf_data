edddadd2bb0f3b33c06a94b59ffb6cd3d130c0e6
billy3321 <billy3321@gmail.com>
2015-12-13 01:57:24 +0800

    fix script error

--- get_year_judgements.sh
+++ get_year_judgements.sh
@@ -7,8 +7,8 @@ FORMAT="%Y-%m-%d"
 start=`$DATE +$FORMAT -d "${1}-01-01"`
 end=`$DATE +$FORMAT -d "${1}-12-31"`
 now=$start
-while [[ "$now" < "$end" ]] ; do
-  now=`$DATE +$FORMAT -d "$now + 1 day"`
+while [[ "$now" <= "$end" ]] ; do
   echo "$now"
   ./get_judgements.rb "$now"
+  now=`$DATE +$FORMAT -d "$now + 1 day"`
 done
