5b8afb9ea4bd711cdfd2cc253131bdc14b2c5e69
billy3321 <billy3321@gmail.com>
2015-12-13 01:59:29 +0800

    fix date error

--- get_year_judgements.sh
+++ get_year_judgements.sh
@@ -5,9 +5,9 @@ cd "$(dirname "$0")"
 DATE=date
 FORMAT="%Y-%m-%d"
 start=`$DATE +$FORMAT -d "${1}-01-01"`
-end=`$DATE +$FORMAT -d "${1}-12-31"`
+end=`$DATE +$FORMAT -d "${1}-12-31 + 1 day"`
 now=$start
-while [[ "$now" <= "$end" ]] ; do
+while [[ "$now" < "$end" ]] ; do
   echo "$now"
   ./get_judgements.rb "$now"
   now=`$DATE +$FORMAT -d "$now + 1 day"`
