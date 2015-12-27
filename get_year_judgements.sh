b676acb36161bbe0954cf7ffb6868fc4d0b3cfb0
billy3321 <billy3321@gmail.com>
2015-12-27 14:26:30 +0800

    fix parse date function

--- get_year_judgements.sh
+++ get_year_judgements.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+# set -o xtrace
 
 cd "$(dirname "$0")"
 
 DATE=date
 FORMAT="%Y-%m-%d"
-start=`$DATE +$FORMAT -d "${1}-01-01"`
-end=`$DATE +$FORMAT -d "${1}-12-31 + 1 day"`
+VAR=$1
+SIZE=${#VAR}
+echo $SIZE
+if [ $SIZE -eq 10 ]; then
+  YEAR=`echo $VAR | cut -d '-' -f 1`
+  start=`$DATE +$FORMAT -d "${VAR}"`
+  end=`$DATE +$FORMAT -d "${YEAR}-12-31 + 1 day"`
+else
+  start=`$DATE +$FORMAT -d "${VAR}-01-01"`
+  end=`$DATE +$FORMAT -d "${VAR}-12-31 + 1 day"`
+fi
+
 now=$start
-echo "start get judgements..." > log/get_judgements-${1}.log
+echo "start get judgements..." > log/get_judgements-${YEAR}.log
 while [[ "$now" < "$end" ]] ; do
   echo "$now"
-  ./get_judgements.rb "$now" >> log/get_judgements-${1}.log 2>&1
+  ./get_judgements.rb "$now" >> log/get_judgements-${YEAR}.log 2>&1
   now=`$DATE +$FORMAT -d "$now + 1 day"`
 done
